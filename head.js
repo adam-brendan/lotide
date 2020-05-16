a18bafe370044cb12ea27e247981c76df828585f
Adam <adamcraveiro@gmail.com>
2020-05-16 18:36:52 -0400

    Implement head function to output first element of array

--- head.js
+++ head.js
@@ -1,6 +1,10 @@
+const head = (arr) => {
+    return arr[0];
+}
+
 const assertEqual = (actual, expected) => {
     return actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
