4ba5deb0ca3c0f4f68fd5abbc57bdc0eac5fbc33
Adam <adamcraveiro@gmail.com>
2020-05-17 19:18:27 -0400

    Implement array checking functions

---
+++ assertArraysEqual.js
@@ -0,0 +1,21 @@
+const assertArraysEqual = (arr1, arr2) => {
+    return eqArrays(arr1, arr2) ? console.log("✅✅✅ These arrays are equal!") : console.log("🛑🛑🛑 These arrays are NOT equal!")
+}
+
+const eqArrays = (arr1, arr2) => {
+    if (arr1.length !== arr2.length) {
+        return false;
+    } else {
+        for (let i = 0; i < arr1.length; i ++) {
+            if (arr1[i] !== arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
