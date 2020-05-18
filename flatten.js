1a540e7fc050199b16243587ef2dc7a71051d39d
Adam <adamcraveiro@gmail.com>
2020-05-17 20:13:09 -0400

    Implement function to flatten arrays of any depth.

---
+++ flatten.js
@@ -0,0 +1,9 @@
+let flatArray = [];
+const flatten = (arr) => {
+    for (item of arr) {
+        Array.isArray(item) ? flatten(item) : flatArray.push(item);
+    }
+    return flatArray;
+}
+
+console.log(flatten([1, 2, [3, 4], 5, [6], [[7, 8], 9, 10]])) // => [1, 2, 3, 4, 5, 6]
