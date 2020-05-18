60ab77edd5c469c4aa868d242295e2439348eaf7
Adam <adamcraveiro@gmail.com>
2020-05-18 16:52:28 -0400

    Implement countLetters function

---
+++ countLetters.js
@@ -0,0 +1,14 @@
+const assertEqual = (actual, expected) => {
+    return actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+
+const countLetters = (str) => {
+    let countObj = {};
+    for (letter of str.replace(/\s+/g, '')) {
+        countObj[letter] = (countObj[letter] + 1) || 1
+    }
+    return countObj;
+}
+
+console.log(countLetters("lighthouse in the house"))
