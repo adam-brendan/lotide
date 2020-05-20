488fec73ff88477fa0c0398e19143f6ac3f77202
Adam <adamcraveiro@gmail.com>
2020-05-20 14:43:01 -0400

    Implement findKey function

---
+++ findKey.js
@@ -0,0 +1,17 @@
+const findKey = (obj, callback) => {
+    for (prop of Object.keys(obj)) {
+        if (callback(obj[prop])) {
+            return prop;
+        }
+    }
+    return undefined;
+}
+
+console.log(findKey({
+    "Blue Hill": { stars: 1 },
+    "Akaleri":   { stars: 3 },
+    "noma":      { stars: 2 },
+    "elBulli":   { stars: 3 },
+    "Ora":       { stars: 2 },
+    "Akelarre":  { stars: 3 }
+  }, x => x.stars === 2))
