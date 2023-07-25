72479406a2ff05cfa79e74c495fdda99af22213a
adam-brendan <adamcraveiro@gmail.com>
2023-07-25 12:15:00 -0400

    Implemented function

--- assertEqual.js
+++ assertEqual.js
@@ -1,6 +1,10 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-
+    if (actual === expected) {
+        console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}.`)
+    } else {
+        console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}.`)
+    }
 };
 
 // TEST CODE
